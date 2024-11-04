e32372e1adf9fef1ac00173ca35283cbec1af6f0
iw72 <iw72@pc7-048-l.cs.st-andrews.ac.uk>
2024-11-04 02:52:27 +0000

    All but todos & testing

--- src/Game.java
+++ src/Game.java
@@ -1,43 +1,95 @@
-public class Game {
+/**
+ * Coordinate: class for expression location in a grid
+*/
+public class Coordinate {
+	private int x;
+	private int y;
+	
+/**
+ * 	Constructor
+ * 	@param row x coordinate 
+ * 	@param column y of coordinate
+*/
+	public Coordinate(int row, int col) {
+		if(row < 0 || col < 0) {
+			throw IndexOutOfBoundsException;	
+		}
+		this.x = row;
+		this.y = col;	
+		
+	}
+
+
+/**
+ * 	Getter for row (x)
+ * 	@return row of this coordinate
+*/
+	public int row() {
+		return x;
+	}
+
+/**
+ * 	Getter for column (y)
+ * 	@return column of this coordinate
+*/
+	public int col() {
+		return y;
+	}
+	
+	@Override
+	public boolean equals(Object obj) {
+		Coordinate objCoordinate = (Coordinate) obj;
+		return this.row() == obj.row() && this.col() == obj.col();
+	}
+	
+	@Override
+	public int hashCode() {
+		return Objects.hash(row, col);
+	}
+}
+
+/**
+ * Grid: class for storing 2d representations
+*/
+public abstract class Grid {
 	public int HEIGHT = 9;
 	public int WIDTH = 7;
 	private int ALL_ROWS[] = getSequence(WIDTH);
 	private int ALL_COLS[] = getSequence(HEIGHT);
-	public int[] WATER_ROWS = {3, 4, 5};
-	public int[] WATER_COLS = {1, 2, 4, 5};
-	public int DEN_COL = 3;	
-		
-	private Player p0;
-	private Player p1;
-	private Square[][]board = new Square[HEIGHT][WIDTH];
-	private Piece[][]pieces = new Piece[HEIGHT][WIDTH];
+	private Object[][] grid = Object[HEIGHT][WIDTH];
 	
 /**
- * 	Constructor for game: creates the board, adds pieces to their starting position
+ * Getter of element at coordinate in grid
 */
-	public Game(Player p0, Player p1) {
-		this.p0 = p0;
-		this.p1 = p1;
-		this.addStartingSquares();
-		this.addStartingPieces();		
+	private Object getGridLocation(Coordinate targetLocation){
+		return grid[targetlocation.col(), targetLocation.row()];
 	}
 
 /**
- * 	Initialises board by setting all squares to plain
- * 	Then adding the three types of special squares
+ * 	Individual coordinate setter for grid
 */	
-	private void addStartingSquares() {
-		//Set the whole board to plainSquares
-		this.bulkSetBoardSquare(PlainSquare::new, ALL_ROWS, ALL_COLS);
-		
-		//Set water squares
-		this.bulkSetBoardSquare(WaterSquare::new, WATER_ROWS, WATER_COLS);
-
-		//Set traps and dens for both players
-		this.addTrapsAndDen(p0);
-		this.addTrapsAndDen(p1);
+	private void setGridLocation(Coordinate targetLocation, Object targetElement) {
+		grid[targetLocation.col(), targetLocation.row()] = targetElement;	
 	}
 
+/**
+ * 	Wrapper for individual coordinate setter to enable large scale setting of grid	
+ * 	@param squareSupplier factory for generating the requested object 
+ * 	@param targetRows 
+ * 	@param targetCols 
+*/
+	private void setGridLocation(int[] targetRows, int[] targetCols, Supplier<? extends Square> elementSupplier) {
+		for (row: targetRows) {
+			for (col: targetCols) {
+				Coordinate targetLocation = new Coordinate(row, col);
+				Object targetElement = elementSupplier.get();
+				this.setGridSquare(targetLocation, targetElement);	
+			}
+		}	
+	}
+/**
+ * 	Helper methods 
+*/	
 	private int[] getSequence(n) {
 		return java.util.stream.IntStream
 			.rangeClosed(0, n)
@@ -45,35 +97,38 @@ public class Game {
 	}
 
 	private int[] getSequence(start, n) {
-		return java.util.stream.IntStream
+		return IntStream
 			.rangeClosed(start, n)
 			.toArray();
 	}
-/**
- * 	Individual square setter for board
-*/	
-	private void setBoardSquare(Coordinate targetLocation, Square targetSquare) {
-		board[targetLocation.col(), targetLocation.row()] = targetSquare;	
-	}
+}
 
 /**
- * 	Bulk square setter for board
- * 	@param squareSupplier factory for generating the requested subclass of Square 
- * 	@param targetRows 
- * 	@param targetCols 
+ * SquareBoard: Grid representation of all the squares
 */
+public abstract class SquareBoard extends Grid {
+
+/**
+ * 	Initialises board by setting all squares to plain
+ * 	Then adding the three types of special squares
+*/
+	public int[] WATER_ROWS = {3, 4, 5};
+	public int[] WATER_COLS = {1, 2, 4, 5};
+	public int DEN_COL = 3;
 	
-	private void bulkSetBoardSquare(Supplier<? extends Square> squareSupplier, int[] targetRows, int[] targetCols) {
-		for (row: targetRows) {
-			for (col: targetCols) {
-				Coordinate targetLocation = new Coordinate(row, col);
-				Square newSquare = squareSupplier.get();
-				this.setBoardSquare(targetLocation, newSquare);	
-			}
-		}	
+	public SquareBoard(Player[] players) {	
+		//Set the whole board to plainSquares
+		this.setGridLocation(ALL_ROWS, ALL_COLS, PlainSquare::new);
+		//Set water squares
+		this.setGridLocation(WATER_ROWS, WATER_COLS, WaterSquare::new);
+
+		//Set traps and dens for all players
+		for(player: players) {
+			this.addTrapsAndDen(player);
+		}
 	}
 /**
- * 	Set den and traps manually for a given player	
+ * 	Set den and traps for a given player	
  * 	@param player 
 */
 	private void addTrapsAndDen(Player player) {
@@ -112,47 +167,247 @@ public class Game {
 		}
 	
 		//Set a full rectangle of traps
-		this.bulkSetBoardSquare(
+		this.setGridLocation(
 			() -> new Trap(player), targetCols, targetRows
 		)
-		//Locate and set den for p0	
-		Coordinate denCoordinateP0 = new Coordinate(denRow, DEN_COL);
-		this.setBoardSquare(denCoordinateP0, new Den(owner));
+		//Locate and set den	
+		Coordinate denCoordinate = new Coordinate(denRow, DEN_COL);
+		this.setGridLocation(denCoordinateP0, new Den(player));
 		//Remove corners
-		this.setBoardSquare(PlainSquare::new, leftCorner);
-		this.setBoardSquare(PlainSquare::new, rightCorner);	
-	}	
-	
-	
+		this.setGridLocation(leftCorner, PlainSquare::new);
+		this.setGridLocation(rightCorner, PlainSquare::new);	
+	}
 }
 
-public class Coordinate {
-	private int x;
-	private int y;
+/**
+ * Game: Grid representation of pieces
+ * Inheritance chain: able to manipulate SquareBoard
+*/
+public class Game extends SquareBoard {
+	private int NUM_PLAYERS = 2;
+	private Player[] players = [NUM_PLAYERS];
+	private Grid pieces = new Grid(HEIGHT, WIDTH);
+	private int lastMoved = 1;
 	
 /**
- * 	Constructor
- * 	@param row x coordinate 
- * 	@param column y of coordinate
+ * 	Constructor for game: creates the board, adds pieces to their starting position
 */
-	public Coordinate(row, col) {
-		this.x = row;
-		this.y = col;	
+	public Game(Player p0, Player p1) {
+		players[0] = p0;
+		players[1] = p1;	
+		//Initialise board with dens
+		super(players);
+	
+		this.addStartingPieces();		
+	}
+	
+	//Special cases: rat (rank 1), tiger (rank 6), lion (rank 7)
+	int RAT_RANK = 1;
+	int TIGER_RANK = 6;
+	int LION_RANK = 7;
+	int[] SPECIAL_RANKS = IntStream.of({RAT_RANK, TIGER_RANK, LION_RANK});
+	
+	public void addPiece(int row, int col, int rank, int playerNumber) {
+		private Piece piece;
+		//Find square at location
+		private targetLocation = new Coordinate(row, col);	
+		private Square square = board.getGridLocation(targetLocation);
+		
+		int specialMatch = SPECIAL_RANKS.anyMatch((e) -> e==rank);
+		if(specialMatch == null){
+			//Base case: not special piece
+			piece = new Piece(owner, square, rank);
+		} else if(specialMatch == RAT_RANK) {
+			//Power 1 piece: rat	
+			piece = new Rat(owner, square);
+		} else if(specialMatch == TIGER_RANK) {
+			//Power 6 piece: tiger
+			piece = new Tiger(owner, square);	
+		} else if(specialMatch == LION_RANK) {
+			//Power 7 piece: lion
+			piece = new Lion(owner, square);
+		}
+
+		pieces.setGridLocation(targetLocation, targetPiece);
+		players[playerNumber].gainOnePiece();
 	}
 
-/**
- * 	Getter for row (x)
- * 	@return row of this coordinate
-*/
-	public int row() {
-		return x;
+	public void addStartingPieces() {
+		int minRank = 1;
+		int maxRank = 8;
+		int[] ranks = getSequence(minRank, maxRank);
+		//TODO add all pieces
+	}
+	
+	
+
+	public Piece getPiece(int row, int col) {
+		private Coordinate targetLocation = new Coordinate(row, col);	
+		return pieces.getGridLocation(targetLocation);
 	}
 
-/**
- * 	Getter for column (y)
- * 	@return column of this coordinate
-*/
-	public int col() {
-		return y;
+	public void move(int fromRow, int fromCol, int toRow, int toCol){
+		//Check if correct player is moving	
+		private Piece fromPiece = this.getPiece(fromRow, fromCol);
+		private Player movingPlayerNumber = getOwner(fromPiece).playerNumber;
+		if(movingPlayerNumber != lastMoved){
+			throw IllegalMoveException();
+		}
+
+		//Check if move is legal
+		private List<Coordinate> legalMoves = getLegalMoves(fromRow, fromCol);
+		private Coordinate toLocation = Coordinate(toRow, toCol);
+		if(!toLocation.contains(legalMoves)) {
+			throw IllegalMoveException();
+		}	
+
+		private Coordinate fromLocation = Coordinate(fromRow, fromCol);
+		
+		//Check if capture is occuring	
+		private toPiece = this.getPiece(toRow, toCol);
+		if(toPiece != null){
+			//Capture
+			toPiece.beCaptured();	
+		}
+
+		//Move piece on board	
+		pieces.setGridLocation(fromLocation, null);
+		pieces.setGridLocation(toLocation, fromPiece);	
+
+		//Move piece's square
+		private Square toSquare = board.getGridLocation(toLocation);
+		fromPiece.move(toSquare);
+
+		lastMoved = movingPlayerNumber;
+		//TODO damaged by traps
 	}
+	
+	public List<Coordinate> getLegalMoves(int row, int col){
+		Coordinate startingLocation = new Coordinate(row, col)	
+		//Guaranteed all structurally possible moves (i.e not out of bounds)	
+		List<Coordinate> legalMoves = getNextNodes(startingLocation);
+		Piece startingPiece = getPiece(row, col);
+		Square startingSquare = board.getGridLocation(startingLocation);
+	
+		List<Coordinate> excludedMoves = [];
+		for(Coordinate move: legalMoves) {
+			Piece targetPiece = getPiece(move.row(), move.col());
+			Square targetSquare = getSquare(move.row(), move.col());
+						
+			boolean exclude = false;
+			while(exclude == false){	
+				//Pieces on the square of the attempted move	
+				boolean targetPiecePresent = targetPiece != null;
+				boolean targetPieceEnemy = getOwner(targetPiece).getPlayerNumber == lastMoved;
+				boolean targetPieceDefeatable = startingPiece.canDefeat(targetPiece);
+				exclude = strongerPieces(targetPiecePresent, targetPieceEnemy, targetPieceDefeatable);
+				exclude = alliedPieces(targetPiecePresent, !targetPieceEnemy, targetPieceDefeatable);
+				
+				//Swimming
+				boolean attemptingSwim = targetSquare.isWater();	
+				boolean canSwim = startingPiece.canSwim();
+				exclude = attemptingSwim && !canSwim;
+			}
+			
+			if exclude == true {
+				excludedMoves.add(move);
+			}
+		}		
+		
+		legalMoves.removeAll(excludedMoves);
+
+
+		//TODO Leaping
+		return legalMoves; 
+	}	
+	
+	private boolean strongerPieces(targetPiecePresent, targetPieceEnemy, targetPieceDefeatable) {
+		return targetPiecePresent && (targetPieceEnemy && !targetPieceDefeatable);
+	}
+	
+	private boolean alliedPieces(targetPiecePresent, targetPieceAllied) { 
+		return targetPiecePresent && targetPieceAllied;
+	}
+
+	//Checker wrapper	
+	private List<>Coordinates excludeMoves(legalMoves, startingPiece, checks) {
+		List<>Coordinates excludedMoves = ArrayList<>Coordinates;
+		for(Coordinate move: legalMoves) {
+			
+			for(function check: checks) {
+				if(!check(startingPiece, targetPiece, targetSquare)) { 
+					excludedMoves.add(move);	
+				}	
+			}	
+		}
+		return legalMoves.removeAll(excludedMoves);	
+	}
+/**
+ * 	Gets all legal moves from a given point	
+ * 	This is essentially a graph traversal problem
+*/
+	private int MOVE_RANGE = 1;
+	private int STEP_SIZE = 1;
+	private List<Coordinate> getNextNodes(Coordinate startingNode) {
+		int currentBudget = MOVE_RANGE;
+		List<Coordinate> affordableNodes = new ArrayList<>();
+		//History is a set to avoid duplicates	
+		Set<Coordinate> history = new HashSet<>();
+	
+		List<Coordinate> queueNodes = new ArrayList<>();
+		queueNodes.add(startingNode);
+	
+		while(currentBudget > 0){
+			List<Coordinate> toBeQueued = new ArrayList<>();	
+			//Traverse all nodes in queue	
+			for(Coordinate node: queueNodes){
+				//Add queue to affordable nodes
+				affordableNodes.add(node);
+
+				//Find all possible neighbours	
+				List<Coordinate> neighbours = Arrays.asList(
+					checkIfValidNode(node.row() - STEP_SIZE, node.col()),
+					checkIfValidNode(node.row() + STEP_SIZE, node.col()),
+					checkIfValidNode(node.row(), node.col() - STEP_SIZE),
+					checkIfValidNode(node.row(), node.col() + STEP_SIZE)
+				);
+				
+				//Process each neighbour
+				for(Coordinate neighbour: neighbours) {
+					//If neighbour is valid and hasn't been traversed before
+					if(neighbour!=null && !history.contains(neighbour)){
+						//Add to be traversed
+						toBeQueued.add(neighbour);
+						//Ensure node can't be retraversed
+						history.add(neighbour);
+					}
+				}
+			}
+			//Update queue	
+			queueNodes = toBeQueued;	
+			//Decrease budget	
+			currentBudget-=STEP_SIZE;
+		}
+
+		return affordableNodes;	
+	}
+
+	private Coordinate checkIfValidNode(int row, int col) {
+		try {
+			return new Coordinate(row, col);
+		} catch (IndexOutOfBoundsException e) {
+			return null;
+		}
+	}
+
+	private Player getOwner(piece) {
+		return players
+			.stream(players)
+			.filter(player -> piece.isOwnedBy(player))
+			.findFirst();
+	}
+}
+
+public class IllegalMoveException extends RuntimeException {
+
 }
--- src/Piece.java
+++ src/Piece.java
@@ -1,6 +1,11 @@
+/**
+ * Regular piece
+*/
+
 public class Piece {
 	private Player owner;
 	private Square square;
+	private int rank;
 	private int strength;
 	private boolean active = true;
 
@@ -10,10 +15,11 @@ public class Piece {
  * 	@param square Square on which piece sits
  * 	@param rank Starting strength of piece 
 */
-	public Piece(Player owner, Square square, int strength) {
+	public Piece(Player owner, Square square, int rank) {
 		this.owner = owner;
 		this.square = square;
-		this.strength = strength;
+		this.rank = rank;	
+		this.strength = rank;
 	}
 
 /**
@@ -23,6 +29,14 @@ public class Piece {
         public boolean isOwnedBy(Player player) {
                 return owner.equals(player);
         }
+	
+	public int getStrength() {
+		return strength;
+	}
+	
+	public int getRank() {
+		return rank;
+	}
 
 	public boolean canSwim() {
 		return false;
@@ -58,4 +72,61 @@ public class Piece {
 		active = false;
 		owner.loseOnePiece();		
 	}	
+}
+
+/**
+ * Special pieces
+*/
+
+public class Rat extends Piece {
+	//Has rank 1
+	private int ratRank = 1;
+	public Rat(Player owner, Square square){
+		super(owner, square, ratRank);
+	}
+
+	//Can swim	
+	@Override
+	public boolean canSwim() {
+		return true;
+	}
+	
+	//Can defeat elephants	
+	@Override
+	public boolean canDefeat(Piece target) {
+		return super.canDefeat(target) || target.getRank()==8;
+	}
+}
+
+public class Tiger extends Piece {
+	//Has rank 6
+	private int tigerRank = 6;
+	public Tiger(Player owner, Square square) {
+		super(owner, square, tigerRank);
+	}
+	
+	//Can leap horizontally only
+	@Override
+	public boolean canLeapHorizontally() {
+		return true;	
+	}
+}
+
+public class Lion extends Piece {
+	//Has rank 7
+	public int lionRank = 7;
+	public Tiger(Player owner, Square square) {
+		super(owner, square, lionRank);
+	}
+
+	//Can leap horizontally and vertically
+	@Override
+	public boolean canLeapHorizontally() {
+		return true;	
+	}
+
+	@Override
+	public boolean canLeapVertically() {
+		return true;	
+	}
 }
